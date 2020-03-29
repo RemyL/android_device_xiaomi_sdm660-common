7e61bd5d54a3e744927284c7f1dff52734ed3eb3
Arian <arian.kulmer@web.de>
2020-03-29 21:46:36 +0200

    sdm660-common: Import antradio permission
    Change-Id: I8ed25ced4a130f843698f97d1a8ec20589f9b18f

--- sdm660.mk
+++ sdm660.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
     libantradio
 
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    $(LOCAL_PATH)/configs/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Biometrics
 PRODUCT_PACKAGES += \
